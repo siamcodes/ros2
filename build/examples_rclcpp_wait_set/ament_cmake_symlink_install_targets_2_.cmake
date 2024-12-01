cc4aefc4a34ac32d718279a2f828d42bd42400cd
siamcodes <siamcodes@gmail.com>
2024-12-01 19:15:37 +0700

    first commit

---
+++ build/examples_rclcpp_wait_set/ament_cmake_symlink_install_targets_2_.cmake
@@ -0,0 +1,1 @@
+ament_cmake_symlink_install_targets("TARGET_FILES" "/home/eleclabs/ros2_ws/build/examples_rclcpp_wait_set/libtalker.so" "/home/eleclabs/ros2_ws/build/examples_rclcpp_wait_set/liblistener.so" "TARGETS" "talker" "listener" "ARCHIVE_DESTINATION" "lib" "LIBRARY_DESTINATION" "lib" "RUNTIME_DESTINATION" "bin")
---
+++ build/pkg_py/build/lib/pkg_py/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from pkg_py.')
+
+
+if __name__ == '__main__':
+    main()
